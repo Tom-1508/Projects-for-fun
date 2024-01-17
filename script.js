fdccc91863b66e4fc2c7d64bc90fec759d89a68e
Tom-1508 <tamalcrj2002@gmail.com>
2024-01-17 22:10:18 +0530

    modified the website a littlebit

---
+++ script.js
@@ -0,0 +1,16 @@
+const twitter_btn = document.querySelector('.ri-twitter-fill');
+
+twitter_btn.onclick = () => {
+    window.location.href = 'https://twitter.com/Majumdar15Tamal';
+}
+
+const about_btn = document.getElementById('about-button')
+about_btn.onclick = () => {
+    alert('clicked');
+}
+
+const arrow_btn = document.querySelector('.ri-linkedin-box-fill');
+
+arrow_btn.onclick = () => {
+    window.location.href = 'https://www.linkedin.com/in/tamal-majumdar-a685a61b4/'
+}
